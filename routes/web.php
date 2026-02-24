9c91b3fca677ac4ea3a8a8208bbd380523512c32
Rifky <rifkypraditya1025@gmail.com>
2026-02-25 06:47:38 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,32 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
 
 Route::get('/world', function () {
     return 'world';
 });
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
 
-Route::get('/about', function () {
-    return '244107020096 - Muhammad Rifky Praditya';
-});
+// Route::get('/about', function () {
+//     return '244107020096 - Muhammad Rifky Praditya';
+// });
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya ' . $name;
@@ -31,10 +37,28 @@ Route::get('/posts/{post}/comments/{comment}', function
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman artikel dengan ID ' . $id;
-});
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Halaman artikel dengan ID ' . $id;
+// });
 
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya ' . $name;
 });
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
